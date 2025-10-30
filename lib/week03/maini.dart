28e4ef77cf7234609e257d873054bb71fefc9a40
choijichan <sjy08007@gmail.com>
2025-10-30 15:10:57 +0900

    Updated Changes

--- lib/week03/maini.dart
+++ lib/week03/maini.dart
@@ -9,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
+}
+  class _HomeScreenState extends State<HomeScreen>{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
